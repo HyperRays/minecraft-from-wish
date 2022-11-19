0809cec422c8c7376dbf4edafa8c6f4857f32dc4
Hyperrays <soham.kuvalekar@stud.edubs.ch>
2022-11-19 20:24:09 +0100

    Fixed global location fn

--- game-logic/chunk_system.py
+++ game-logic/chunk_system.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from prelude import *
+import itertools
 
 
 window.init((300,300),"test objects")
@@ -13,35 +14,29 @@ class Chunk(GraphicsObject):
 
     # chunks have their own x,y coordinates, in the chunks there is another grid
     # this grid is the local coordinates of the individual tiles
-    # get_global_coordinates() gets the global coordinates of the tile
+    # *() gets the global coordinates of the tile
     # get_local_coordinates() local coordinates of the tile
     # get_chunk() gets the chunk from a global coordinates
 
-    def __init__(self, vec: vec2d) -> None:
+    def __init__(self, vec: vec2d, mapping_func) -> None:
+        super().__init__()
         # chunks do not get immediately added to the objects list, but get loaded in 
         # dynamicaly depending on the players coordinates
-        self.internal_objects = [[None]*self.__chunk_size]*self.__chunk_size
+        self.internal_objects = [[[None] for _ in range(self.__chunk_size)] for _ in range(self.__chunk_size)]
         assert(vec.x%1==0 and vec.y%1==0)
         self.__chunk_dict.update({vec: self})
         self.position = vec
+        self.on_load(mapping_func)
 
     
-    def get_global_coordinates(self, vec: vec2d) -> vec2d:
+    def get_chunk_coordinates(self, vec: vec2d) -> vec2d:
         """
         global coordinates based on current chunk and coordinates
         """
         # a bit of branchless programming
-        return vec2d(   x= (vec.x>0)*(
-                                self.__chunk_size*self.position.x + vec.x)
-                                +
-                            (vec.x<0)*(
-                                self.__chunk_size*self.position.x - vec.x),
+        return vec2d(   x= self.__chunk_size*self.position.x+vec.x,
 
-                        y = (vec.y>0)*(
-                                self.__chunk_size*self.position.x + vec.y)
-                            +
-                            (vec.y<0)*(
-                                self.__chunk_size*self.position.x - vec.y)
+                        y = self.__chunk_size*self.position.y+vec.y
                     )
     
     def get_local_position(self, vec: vec2d):
@@ -50,5 +45,21 @@ class Chunk(GraphicsObject):
         """
 
         vec2d(x=vec.x%self.__chunk_size ,y= vec.y%self.__chunk_size)
+    
+    def update(self):
+        for object in list(itertools.chain(*self.internal_objects)):
+            try:
+                object.update()
+            except AttributeError:
+                pass
 
+        
+
+    def on_load(self, mapping_func):
+        # I don't know if this function is here to stay
+        # probably need to find a better way
+
+        for x, obj_x in enumerate(self.internal_objects):
+            for y, obj in enumerate(self.internal_objects):
+                self.internal_objects[x][y] = mapping_func(x,y, self)
 
