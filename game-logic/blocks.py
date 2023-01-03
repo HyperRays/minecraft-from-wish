2508a535f8f6d8e550b60210982def2e4e27ce6d
HyperRays <soham.kuvalekar@stud.edubs.ch>
2023-01-03 13:54:21 +0100

    better blocks

--- game-logic/blocks.py
+++ game-logic/blocks.py
@@ -8,14 +8,14 @@ collider_n = "collider"
 
 class Material:
     
-    AIR = "Air"
-    ICE = "Ice"
-    SAND = "Sand"
-    GRASS = "Grass"
-    DIRT = "Dirt"
-    WATER = "Water"
-    STONE = "Stone"
-    SNOW = "Snow"
+    AIR = 1 << 0
+    ICE = 1 << 1
+    SAND = 1 << 2
+    GRASS = 1 << 3
+    DIRT = 1 << 4
+    WATER = 1 << 5
+    STONE = 1 << 6
+    SNOW = 1 << 7
 
     @staticmethod
     def map(material):
