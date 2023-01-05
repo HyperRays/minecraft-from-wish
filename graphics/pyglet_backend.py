e450a5a87bc614428fb4625e9241629c6efd74e9
HyperRays <soham.kuvalekar@stud.edubs.ch>
2023-01-05 19:10:11 +0100

    put event loop in only class

--- graphics/pyglet_backend.py
+++ graphics/pyglet_backend.py
@@ -3,28 +3,18 @@ import os
 from typing import Any
 import pyglet
 
-def __event_loop(window: pyglet.window.Window, cls, update_closure: Any, input_closure: Any, render_closure: Any):
-    loop = asyncio.get_event_loop()
-    @window.event
-    def on_draw():
-        loop.run_until_complete(update_closure(cls))
-        loop.run_until_complete(render_closure(cls))
-        window.clear()
-    
-    # @window.event
-    def on_key_press(symbol, modifiers):
-        loop.run_until_complete(input_closure(cls, symbol))
-    
-    pyglet.app.run()
 
 class PygletBackend:
     backend = "pyglet"
     window: pyglet.window.Window | None = None
     size: tuple[float, float] | None = None
 
+    # groups are used like layers
+    # https://pyglet.readthedocs.io/en/latest/modules/graphics/index.html
+    groups: list[pyglet.graphics.Group] = []
+
     @staticmethod
     def load_texture(path):
-        fullname = os.path.join("../assets/images", path)
         fullname = os.path.join("../assets/images", path)
         return pyglet.image.load(fullname)
 
@@ -33,3 +23,18 @@ class PygletBackend:
 
         cls.window = pyglet.window.Window(size[0], size[1], title)
         cls.size = size
+
+    @classmethod
+    async def event_loop(cls, update_closure: Any, input_closure: Any, render_closure: Any) -> None:
+        loop = asyncio.get_event_loop()
+        @cls.window.event
+        def on_draw():
+            loop.run_until_complete(update_closure(cls))
+            loop.run_until_complete(render_closure(cls))
+            cls.window.clear()
+        
+        # @window.event
+        def on_key_press(symbol, modifiers):
+            loop.run_until_complete(input_closure(cls, symbol))
+        
+        pyglet.app.run()
